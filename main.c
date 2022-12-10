df2c107f47912b305f5ba630b01e2a8b0e1b3611
Maurlcio <thecoolermess@gmail.com>
2022-12-09 16:27:51 -0800

    Dealt with every single valgrind issue I could find. No more 'definitely lost', and thankfully not even a 'still reachable'. hell yeah.

--- main.c
+++ main.c
@@ -1,30 +1,29 @@
 #include "shell.h"
+
 /**
+ * main - extremely basic interpretation of a standard UNIX shell
  *
+ * @ac: argument count. Well, would be anyway.
+ * @argv: array of arguments passed to the function. tokenized.
  *
- *
- *
+ * Return: 0 on success. -1 on edge case.
  **/
-int main(int ac, char **argv)
+
+int main(__attribute__((unused))int ac, char **argv)
 {
 	char *prompt = "(S_shell) $ ", *command;
 	char *lineptr = NULL, *lineptr_copy = NULL, *token;
 	size_t n = 0;
-	ssize_t nchars_read; /* number of characters the users types */
+	ssize_t nchars_read;
 	const char *delim = " \n";
 	int num_tokens = 0, i, fre, st;
 	pid_t myPID;
 	extern char **environ;
 
-	/*declaring void variables*/
-	(void)ac;
-
-	/*loop cration for the shell prompt */
 	while (1)
 	{
 		printf("%s", prompt);
 		nchars_read = getline(&lineptr, &n, stdin);
-		/* check if the getline function failed or reached EOF or user use CTRL + D */ 
 		if (nchars_read == -1)
 		{
 			printf("\nEOF reached.\n");
@@ -32,19 +31,15 @@ int main(int ac, char **argv)
 			return (-1);
 		}
 
-		/* allocate space for a copy of the lineptr */
 		lineptr_copy = malloc(sizeof(char) * nchars_read);
 		if (lineptr_copy == NULL)
 		{
 			perror("Unable to allocate memory.");
 			free(lineptr);
 			return (-1);
-		}
-		/* copy lineptr to lineptr_copy */
+		}	
 		strcpy(lineptr_copy, lineptr);
 
-		/* split the string (lineptr) into an array of words */
-		/* calculate the total number of tokens */
 		token = strtok(lineptr, delim);
 
 		while (token != NULL)
@@ -54,10 +49,8 @@ int main(int ac, char **argv)
 		}
 		num_tokens++;
 
-		/* Allocate space to hold the array of strings */
 		argv = malloc(sizeof(char *) * num_tokens);
 
-		/* Store each token in the argv array */
 		token = strtok(lineptr_copy, delim);
 
 		for (i = 0; token != NULL; i++)
@@ -80,15 +73,19 @@ int main(int ac, char **argv)
 			free(argv);
 			exit(EXIT_SUCCESS);
 		}
-
-        	/* execute the command */
         	myPID = fork();
-
 		if (myPID == 0)
 		{
 			if (execve(command, argv, environ) == -1)
+			{
 				perror("Error");
-			exit(EXIT_FAILURE);
+				free(lineptr);
+				free(lineptr_copy);
+				for (fre = 0; fre <= i;fre++)
+					free(argv[fre]);
+				free(argv);
+				exit(EXIT_FAILURE);
+			}
 		}
 		else if (myPID == -1)
 			perror("Error");
@@ -97,16 +94,11 @@ int main(int ac, char **argv)
 			do {
 				myPID = waitpid(myPID, &st, WUNTRACED);
 			} while (!WIFEXITED(st) && !WIFSIGNALED(st));
+			free(lineptr_copy);
+			for (fre = 0; fre <= i; fre++)
+				free(argv[fre]);
+			free(argv);	
 		}
-
-		for (fre = 0; fre <= i; fre++)
-			free(argv[fre]);
-		free(argv);
 	}
-
-	/* free up allocated memory */
-	free(lineptr_copy);
-	free(lineptr);
-
 	return (0);
 }
